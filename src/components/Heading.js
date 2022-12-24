c89bacb7ab792b52a88a4297628b50dc00c94199
Ankit <ankit.saxena023@gmail.com>
2022-12-24 10:35:40 +0530

    remove my name

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 function Heading() {
     return (
-        <>
-        <h1>Welcome to our Site.</h1>
-        <h2>My name is Ankit Saxena</h2>
-        
-        </>
+        <h1>Welcome to our Site.</h1>        
     )
 }
 
