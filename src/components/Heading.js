9ddf6aa99e7b121c4f4de54bd57dd16e7dfa8c2b
Ankit <ankit.saxena023@gmail.com>
2022-12-24 10:26:46 +0530

    Added my name in Heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 function Heading() {
     return (
+        <>
         <h1>Welcome to our Site.</h1>
+        <h2>My name is Ankit Saxena</h2>
+        
+        </>
     )
 }
+
+export default Heading;
